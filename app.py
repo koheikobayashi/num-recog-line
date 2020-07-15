90a70e0a363fcebbeb8d956107f947dc9c17cbf2
Hiroaki-Ozaki <53843449+Hiroaki-Ozaki@users.noreply.github.com>
2020-07-15 14:39:32 +0900

    new push

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, request, abort
+
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.exceptions import (InvalidSignatureError)
+from linebot.models import (MassageEvent, TextMessage, TextSendMessage)
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'HelloWorld!'
+ACCESS_TOKEN = "Sdr1mE+hoLx7RXtdreaAegInEBX33ktIrmNbvCG3bj64ELcpMWzJP7KfbgCK8voTTEvWdb/scFfYSaIeUaUUNocxDUAbb+rwoZBphwVpRF/mhlE+bt3dGrQ/5rlTY2+IhwwoWTpdHdr7v1CbJ7AeEgdB04t89/1O/w1cDnyilFU="
+SECRET = "a7dce377ee7c51a6418d97ea36277175"
+
+
+line_bot_api = LineBotApi(ACCESS_TOKEN)
+handler = WebhookHandler(SECRET)
+@app.route("/callback",methods=['POST'])
+def callback():
+    signature = request.headers['XLineSignature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Requestbody: " + body)
+    try:
+        handler.handle(body,signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return'OK'
+
+
+@handler.add(MessageEvent,message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+
 
 if __name__ == "__main__":
     app.run()
