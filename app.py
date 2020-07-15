63adb7bb91b9f9704035fa09e01dff903cd469a2
Hiroaki-Ozaki <53843449+Hiroaki-Ozaki@users.noreply.github.com>
2020-07-15 14:57:10 +0900

    aa

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from linebot.models import (MessageEvent,TextMessage,TextSendMessage)
 app = Flask(__name__)
 
 ACCESS_TOKEN = "Sdr1mE+hoLx7RXtdreaAegInEBX33ktIrmNbvCG3bj64ELcpMWzJP7KfbgCK8voTTEvWdb/scFfYSaIeUaUUNocxDUAbb+rwoZBphwVpRF/mhlE+bt3dGrQ/5rlTY2+IhwwoWTpdHdr7v1CbJ7AeEgdB04t89/1O/w1cDnyilFU="
-SECRET = "a7dce377ee7c51a6418d97ea36277175"
+SECRET = "d1fec2f308c69fe2ff66481137cd3d64"
 
 
 line_bot_api = LineBotApi(ACCESS_TOKEN)
@@ -20,7 +20,7 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Requestbody: " + body)
     try:
-        handler.handle(body,signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
 
@@ -29,7 +29,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
 
 
 if __name__ == "__main__":
