219c737177f2b9243948cb81f3b0afe72099aac4
小林耕平 <>
2021-04-21 16:19:42 +0900

    add 4 imagas

--- app.py
+++ app.py
@@ -48,15 +48,44 @@ def handle_image_message(event):
     
     img = Image.open("static/"+event.message.id+".jpg")
     img_resize = img.resize((400, 100))
-    img_resize.crop((0, 0, 100, 100)).save("static/"+event.message.id+"-test.jpg", quality=95)
-    img = image.load_img("static/"+event.message.id+"-test.jpg", grayscale=False, target_size=(28, 28))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = x / 255.0
+    img_resize.crop((0, 0, 100, 100)).save("static/"+event.message.id+"-1.jpg", quality=95)
+    img_resize.crop((100, 0, 200, 100)).save("static/"+event.message.id+"-2.jpg", quality=95)
+    img_resize.crop((200, 0, 300, 100)).save("static/"+event.message.id+"-3.jpg", quality=95)
+    img_resize.crop((300, 0, 400, 100)).save("static/"+event.message.id+"-4.jpg", quality=95)
+    img1 = image.load_img("static/"+event.message.id+"-1test.jpg", grayscale=False, target_size=(28, 28))
+    img2 = image.load_img("static/"+event.message.id+"-2test.jpg", grayscale=False, target_size=(28, 28))
+    img3 = image.load_img("static/"+event.message.id+"-3test.jpg", grayscale=False, target_size=(28, 28))
+    img4 = image.load_img("static/"+event.message.id+"-4test.jpg", grayscale=False, target_size=(28, 28))
+
+    x1 = image.img_to_array(img1)
+    x1 = np.expand_dims(x1, axis=0)
+    x1 = x1 / 255.0
     model = load_model('color_model.h5')
-    result_predict = model.predict_classes(x)
-    n = num[result_predict[0]]
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=n))
+    result_predict = model.predict_classes(x1)
+    n1 = num[result_predict[0]]
+
+    x2 = image.img_to_array(img2)
+    x2 = np.expand_dims(x2, axis=0)
+    x2 = x2 / 255.0
+    model = load_model('color_model.h5')
+    result_predict = model.predict_classes(x2)
+    n2 = num[result_predict[0]]
+
+    x3 = image.img_to_array(img3)
+    x3 = np.expand_dims(x3, axis=0)
+    x3 = x3 / 255.0
+    model = load_model('color_model.h5')
+    result_predict = model.predict_classes(x3)
+    n3 = num[result_predict[0]]
+
+    x4 = image.img_to_array(img4)
+    x4 = np.expand_dims(x4, axis=0)
+    x4 = x4 / 255.0
+    model = load_model('color_model.h5')
+    result_predict = model.predict_classes(x4)
+    n4 = num[result_predict[0]]
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=n1+n2+n3+n4))
 
 
 if __name__ == "__main__":
