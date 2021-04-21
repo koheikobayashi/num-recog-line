76ba211518816bfebb8172b11666b777aa6a35e5
小林耕平 <>
2021-04-21 15:17:51 +0900

    fix keras

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (InvalidSignatureError)
 from linebot.models import (MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage)
 
 from keras.preprocessing import image
-from tensorflow.keras.models import load_model
+from keras.models import load_model
 from PIL import Image
 
 
